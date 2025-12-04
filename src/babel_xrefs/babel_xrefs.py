4d04e2ae01a768b358a5f6d918edc6d8d442aca1
Gaurav Vaidya <gaurav@ggvaidya.com>
2025-12-03 23:02:21 -0500

    Added a method to look up a particular identifier.

--- src/babel_xrefs/babel_xrefs.py
+++ src/babel_xrefs/babel_xrefs.py
@@ -11,6 +11,28 @@ class BabelXRefs:
     def __init__(self, downloader: BabelDownloader):
         self.downloader = downloader
 
+    def get_curie_ids(self, curies: list[str]):
+        """
+        Search for all identifiers in the /ids/ files for a particular CURIE.
+
+        :param curie: A CURIE to search for.
+        :return: A list of cross-references containing that CURIE.
+        """
+
+        identifier_parquet = self.downloader.get_downloaded_file('duckdb/Identifiers.parquet')
+        concord_metadata_parquet = self.downloader.get_downloaded_file('duckdb/Metadata.parquet')
+
+        # Query the Parquet files using DuckDB.
+        duckdb_path = self.downloader.get_output_file('output/duckdbs/xrefs.duckdb')
+        db = duckdb.connect(duckdb_path)
+        identifier_table = db.read_parquet(identifier_parquet)
+        xrefs = db.execute(f"SELECT * FROM identifier_table WHERE curie IN $1", [curies])
+
+        # TODO: convert into case classes.
+
+        return xrefs.fetchall()
+
+
     def get_curie_xrefs(self, curies: list[str]):
         """
         Search for all identifiers that are cross-referenced to the given CURIE.
@@ -20,7 +42,7 @@ class BabelXRefs:
         """
 
         concord_parquet = self.downloader.get_downloaded_file('duckdb/Concord.parquet')
-        concord_metadata_parquet = self.downloader.get_downloaded_file('duckdb/ConcordMetadata.parquet')
+        concord_metadata_parquet = self.downloader.get_downloaded_file('duckdb/Metadata.parquet')
 
         # Query the Parquet files using DuckDB.
         duckdb_path = self.downloader.get_output_file('output/duckdbs/xrefs.duckdb')
--- src/babel_xrefs/cli.py
+++ src/babel_xrefs/cli.py
@@ -1,7 +1,6 @@
 # Command line interface for babel-xrefs
 import click
 import logging
-import babel_xrefs
 from babel_xrefs.core.downloader import BabelDownloader
 from babel_xrefs.babel_xrefs import BabelXRefs
 
@@ -34,6 +33,31 @@ def xrefs(curies: list[str], babel_url: str, local_dir: str):
 
     bxref = BabelXRefs(BabelDownloader(babel_url, local_path=local_dir))
     xrefs = bxref.get_curie_xrefs(curies)
+    for xref in xrefs:
+        print(xref)
+
+@cli.command("ids")
+@click.argument("curies", type=str, required=True, nargs=-1)
+@click.option("--local-dir", type=str, default="data/2025nov19", help="Local location to save Babel download files to")
+@click.option("--babel-url", type=str, default="https://stars.renci.org:443/var/babel_outputs/2025nov19/", help="Base URL of the Babel server")
+def ids(curies: list[str], babel_url: str, local_dir: str):
+    """
+    Fetches and prints the ID records for the given CURIEs, along with Biolink type if provided.
+
+    \f
+
+    :param curies: A list of CURIEs (Compact URI) for which cross-references need
+        to be retrieved.
+    :type curies: list[str]
+    :param babel_url: Base URL of the Babel server
+    :type babel_url: str
+
+    :return: None
+    """
+    logging.basicConfig(level=logging.INFO)
+
+    bxref = BabelXRefs(BabelDownloader(babel_url, local_path=local_dir))
+    xrefs = bxref.get_curie_ids(curies)
     for xref in xrefs:
         print(xref)
 
